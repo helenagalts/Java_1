b6c1ac8242025698b10d708d3b5a410c54b19099
lenagalts <lenagalts@gmail.com>
2019-03-09 09:41:30 +0300

    Задача 8

---
+++ src/Task_8.java
@@ -0,0 +1,20 @@
+public class Task_8 {
+    
+    public static void main(String args[])
+
+    {
+
+        for(int x = 1; x <= 50; x++)
+
+        {
+
+            System.out.print("Значение x: " + x );
+
+            System.out.print("\n");
+
+        }
+
+    }
+
+}
+
