605ca7486aa39469c1fd176f5f917b48bf85ac9e
lenagalts <lenagalts@gmail.com>
2019-03-10 13:20:43 +0300

    Задача 9

---
+++ src/Task_9.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Task_9 {
+    public static void main(String[] args) {
+
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите количество элементов массива:");
+        int size = in.nextInt();
+        int array[] = new int[size];
+        System.out.println("Введите элементы массива:");
+        for (int i = 0; i < size; i++) {
+            array[i] = in.nextInt();
+        }
+        for (int i = 0; i < size; i++) {
+            array[i] = array[i] * 2;
+        }
+        System.out.println("Элементы массива, умноженные на 2:");
+        for (int i = 0; i < size; i++) {
+            System.out.println(array[i]);
+        }
+
+
+
+
+    }
+
+}
