e398b1d110366154bd391e242a1452ffa3146ab6
lenagalts <lenagalts@gmail.com>
2019-03-10 23:02:54 +0300

    Задача 11

---
+++ src/Task_11.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Task_11 {
+
+        public static void main(String args[])
+
+        {
+            Scanner in = new Scanner(System.in);
+            System.out.println( "Введите строку:");
+            String str = in.nextLine();
+            System.out.println( "Ваша строка: " + str);
+
+        }
+}
